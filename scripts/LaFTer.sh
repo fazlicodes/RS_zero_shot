bb4bbad7a1c2bd8819142e755e1617adde50be61
Mohamed Imam <fazlimaam@gmail.com>
2023-12-30 23:50:24 +0400

    vision encoder saving folder creation

--- scripts/LaFTer.sh
+++ scripts/LaFTer.sh
@@ -4,6 +4,7 @@ DATA=data
 TRAINER=LaFTer
 CFG=vit_b32
 dset="$1"
+v_encoder=clip
 txt_cls=lafter
 CUDA_VISIBLE_DEVICES=0 python LaFTer.py \
 --root ${DATA} \
@@ -12,6 +13,6 @@ CUDA_VISIBLE_DEVICES=0 python LaFTer.py \
 --epochs 50 \
 --dataset-config-file configs/datasets/"${dset}".yaml \
 --config-file configs/trainers/text_cls/${CFG}.yaml \
---output-dir output/${TRAINER}/${CFG}/"${dset}-onval_set" \
+--output-dir output/${TRAINER}/${CFG}/"${dset}/${v_encoder}" \
 --lr 0.0005 \
 --txt_cls ${txt_cls}
