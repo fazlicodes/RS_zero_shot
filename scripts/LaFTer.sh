019b6a901280f31b44635f0c6589367b4dd624e9
sanoojan <sanoojan24@gmail.com>
2024-01-09 20:24:37 +0400

    minor changes for exp

--- scripts/LaFTer.sh
+++ scripts/LaFTer.sh
@@ -2,21 +2,23 @@
 # custom config
 DATA=data
 TRAINER=LaFTer
-CFG=vit_b32 #'GeoRSCLIP' or 'vit_b32' or 'RemoteCLIP' or 'EVA02_CLIP_B_psz16_s8B'
+CFG=GeoRSCLIP #'GeoRSCLIP' or 'vit_b32' or 'RemoteCLIP' or 'EVA02_CLIP_B_psz16_s8B'
 dset="$1"
 txt_cls=lafter
 nos_epochs=50
-bws="None" # 'conf_alpha' or 'fixed_alpha_{value}' with alpha rate or 'avg'
-CUDA_VISIBLE_DEVICES=0 python LaFTer.py \
+bws='avg' # 'conf_alpha' or 'fixed_alpha_{value}' with alpha rate or 'avg'
+CUDA_VISIBLE_DEVICES=3 python LaFTer.py \
 --root ${DATA} \
 --trainer ${TRAINER} \
 --dataset-config-file configs/datasets/"${dset}".yaml \
 --config-file configs/trainers/text_cls/${CFG}.yaml \
---output-dir output_VEs_unshared_ln_learnable_textdesc/${TRAINER}/${CFG}/"${dset}"_"${nos_epochs}"/"${bws}_LN_Frozen" \
+--output-dir output_LearnTE_VEsunshared_textdesc/${TRAINER}/${CFG}/"${dset}"_"${nos_epochs}"/"${bws}_LN_Frozen" \
 --lr 0.0005 \
 --epochs ${nos_epochs} \
 --txt_cls ${txt_cls} \
 --bws ${bws} \
---batch_size 50
+--batch_size 50 \
+--train_text_ln \
+# --text_only 
 # --ln_frozen \
 # echo "output_3/${TRAINER}/${CFG}/"${dset}"_"${nos_epochs}"/"${bws}_LN_Frozen""
