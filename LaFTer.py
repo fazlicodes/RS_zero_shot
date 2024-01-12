e878cfcb2040cff53c3136587fc485d45517ccfd
sanoojan <sanoojan24@gmail.com>
2024-01-12 16:12:11 +0400

    early stopping code

--- LaFTer.py
+++ LaFTer.py
@@ -237,6 +237,10 @@ def train_lafter(args, model, tr_loader, val_loader, test_loader=None):
     # df = pd.DataFrame(columns=columns)
     df_to_append = []
 
+    # Initialize early stopping parameters
+    early_stopping_counter = 0
+    early_stopping_threshold = 25
+
     for epoch in range(args.epochs):
         print(f'Epoch: {epoch}')
         model.eval()
@@ -363,6 +367,7 @@ def train_lafter(args, model, tr_loader, val_loader, test_loader=None):
         if val_acc>best_acc:
             best_val_acc="Yes"
             best_acc=val_acc
+            early_stopping_counter = 0
             print('------------')
             print("Best Epoch ", epoch)
             print("Best Val acc", val_acc)
@@ -372,11 +377,16 @@ def train_lafter(args, model, tr_loader, val_loader, test_loader=None):
 
             #Save the whole model
             torch.save(model.state_dict(), os.path.join(args.output_dir, "model_best.pth")) 
+        else:
+            early_stopping_counter += 1
         
         print("Output dir: ",args.output_dir)
 
         df_to_append.append([epoch, ps_text_acc, ps_zs_acc, total_loss.avg, val_acc, best_test_acc, best_val_acc])
         print("Output dir: ",args.output_dir)
+        if early_stopping_counter >= early_stopping_threshold:
+            print(f'Early stopping at epoch {epoch} due to no improvement in validation accuracy.')
+            break
     df = pd.DataFrame(df_to_append, columns=columns)    
     csv_path = os.path.join(args.output_dir, "training_metrics.csv")
     df.to_csv(csv_path, index=False)
