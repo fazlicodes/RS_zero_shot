a12305bd3a834f76cc13eab63a82b46cfec696d9
sanoojan <sanoojan24@gmail.com>
2024-01-15 12:14:57 +0400

    removed saving embeddings

--- scripts/LaFTer.sh
+++ scripts/LaFTer.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 # custom config
+rm -rf embeddings/
 DATA=data
 TRAINER=LaFTer
 CFG=GeoRSCLIP #'GeoRSCLIP' or 'vit_b32' or 'RemoteCLIP' or 'EVA02_CLIP_B_psz16_s8B'
 dset="$1"
 txt_cls=lafter
-nos_epochs=50
-bws="avg" # 'conf_alpha' or 'fixed_alpha_{value}' with alpha rate or 'avg'
+nos_epochs=10
+bws="None" # 'conf_alpha' or 'fixed_alpha_{value}' with alpha rate or 'avg'
 CUDA_VISIBLE_DEVICES=0 python LaFTer.py \
 --root ${DATA} \
 --trainer ${TRAINER} \
 --dataset-config-file configs/datasets/"${dset}".yaml \
 --config-file configs/trainers/text_cls/${CFG}.yaml \
---output-dir output_3/${TRAINER}/${CFG}/"${dset}"_"${nos_epochs}"/"${bws}_LN_Frozen" \
+--output-dir test_again/${TRAINER}/${CFG}/"${dset}"_"${nos_epochs}"/"${bws}_LN_Frozen" \
 --lr 0.0005 \
 --epochs ${nos_epochs} \
 --txt_cls ${txt_cls} \
 --bws ${bws} \
---ln_frozen \
---batch_size 50
+--batch_size 50 \
+--text_only
+# --ln_frozen \
+
